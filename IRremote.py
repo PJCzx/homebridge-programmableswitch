59b6b1be9f7294b8c07964e07cfe9b60d840bc5d
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2017-11-05 16:53:38 +0100

    Indentation + workaround

--- IRremote.py
+++ IRremote.py
@@ -9,13 +9,16 @@ def IRremoteSend(remote, key):
 		os.system("sudo lircd -d /dev/lirc0")
 
 	cmd = "irsend SEND_ONCE %s %s" % (remote, key)
-	os.system(cmd)
+	#os.system(cmd)
+        for n in '123':
+		os.system(cmd)
+		time.sleep(1)
 
-	if remote == "VP":
+        if remote == "VP":
 		if key == "KEY_POWER":
 			for n in '123':
-			os.system(cmd)
-			time.sleep(1)
+				os.system(cmd)
+				time.sleep(1)
 
 if __name__ == '__main__':
     IRremoteSend(sys.argv[1], sys.argv[2])
