d937494ab4f72e3e77eb992faa14265da9c9220b
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2017-11-05 16:46:06 +0100

    Send 3 times only fo VP ON/OFF

--- IRremote.py
+++ IRremote.py
@@ -9,14 +9,13 @@ def IRremoteSend(remote, key):
 		os.system("sudo lircd -d /dev/lirc0")
 
 	cmd = "irsend SEND_ONCE %s %s" % (remote, key)
-       	for n in '123':
-		os.system(cmd)
-		time.sleep(1)
+	os.system(cmd)
 
 	if remote == "VP":
 		if key == "KEY_POWER":
+			for n in '123':
+			os.system(cmd)
 			time.sleep(1)
-			os.system("irsend SEND_ONCE VP KEY_POWER")
 
 if __name__ == '__main__':
     IRremoteSend(sys.argv[1], sys.argv[2])
